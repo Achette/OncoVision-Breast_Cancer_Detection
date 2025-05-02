8572e43cd6a777cadddf93fb93628c63517b2b27
Achette <i.achette@live.com>
2025-05-02 15:37:21 -0300

    feat: cria layout de página de login

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,95 @@
-import { Box, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Heading,
+  VStack,
+  Field,
+  Input,
+  Button,
+  HStack,
+  Text,
+  Link as ChakraLink,
+} from '@chakra-ui/react'
+import Link from 'next/link'
 
 export default function Login() {
   return (
-    <Box>
-      <Text color="primary">Esta é a página de login</Text>
+    <Box
+      as="form"
+      w="31.25rem"
+      h="27.375rem"
+      p="1.875rem"
+      bgColor="light"
+      borderRadius="1rem"
+      boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)"
+    >
+      <VStack gap="1.5rem">
+        <Heading as="h2" color="primary" fontSize="1.5rem" fontWeight={700}>
+          Acesse sua conta
+        </Heading>
+
+        <Box w="full">
+          <Field.Root required>
+            <Field.Label color="dark" fontSize="1rem">
+              E-mail <Field.RequiredIndicator />
+            </Field.Label>
+            <Input
+              placeholder="Seu email"
+              type="email"
+              borderColor="input"
+              color="secondary"
+            />
+            <Field.HelperText></Field.HelperText>
+          </Field.Root>
+        </Box>
+
+        <Box w="full">
+          <Field.Root required>
+            <Field.Label color="dark" fontSize="1rem">
+              Senha <Field.RequiredIndicator />
+            </Field.Label>
+            <Input
+              placeholder="Seu senha"
+              type="password"
+              borderColor="input"
+              color="secondary"
+            />
+            <Field.HelperText></Field.HelperText>
+          </Field.Root>
+        </Box>
+
+        <HStack
+          w="full"
+          color="primary"
+          justifyContent="flex-end"
+          fontSize="0.875rem"
+        >
+          Esqueceu sua senha?
+        </HStack>
+
+        <Button
+          w="full"
+          bgColor="primary"
+          color="light"
+          type="submit"
+          _hover={{ bgColor: 'primaryDark' }}
+        >
+          Entrar
+        </Button>
+
+        <Box>
+          <Text color="dark">
+            Não tem uma conta?{' '}
+            <ChakraLink
+              as={Link}
+              href="/sign-up"
+              color="primary"
+              fontWeight={600}
+            >
+              Cadastre-se
+            </ChakraLink>
+          </Text>
+        </Box>
+      </VStack>
     </Box>
   )
 }
