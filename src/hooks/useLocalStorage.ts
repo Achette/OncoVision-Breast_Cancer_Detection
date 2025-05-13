7251e877469fc223163927d43d5158bce13aa4e3
Achette <i.achette@live.com>
2025-05-13 20:07:42 -0300

    feat: cria hook para gerenciar localStorage

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,13 +1,47 @@
-import { TOKEN_KEY } from "@/constants/system"
+'use client'
+import { TOKEN_KEY, USER_KEY } from '@/constants/system'
 
+/* 
+    HASH
+*/
 export const saveToken = (token: string) => {
-  localStorage.setItem(TOKEN_KEY, token)
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(TOKEN_KEY, token)
+  }
 }
 
 export const getToken = (): string | null => {
-  return localStorage.getItem(TOKEN_KEY)
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(TOKEN_KEY)
+  }
+  return null
 }
 
 export const removeToken = () => {
-  localStorage.removeItem(TOKEN_KEY)
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem(TOKEN_KEY)
+  }
+}
+
+/*
+    USER
+*/
+
+export const saveUser = (token: string) => {
+  if (typeof window !== 'undefined') {
+    localStorage.setItem(USER_KEY, token)
+  }
+}
+
+export const getUser = (): string | null => {
+  if (typeof window !== 'undefined') {
+    return localStorage.getItem(USER_KEY)
+  }
+  return null
+}
+
+export const removeUser = () => {
+  if (typeof window !== 'undefined') {
+    localStorage.removeItem(USER_KEY)
+  }
 }
