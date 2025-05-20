720be7f5a11c43267f6b902e583707ce8c4c5416
Achette <i.achette@live.com>
2025-05-20 10:18:42 -0300

    feat: integra form com service para fazer a predição

--- src/components/predctionForm/index.tsx
+++ src/components/predctionForm/index.tsx
@@ -1,18 +1,66 @@
 'use client'
 
+import { useCallback, useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { AppDispatch } from '@/redux/store'
 import { Box, Button, Field, Flex, Heading, Input } from '@chakra-ui/react'
+import { PredictDataState } from '@/models'
+import { predict } from '@/redux/reducer/predict'
 
 export const PredictionForm = () => {
+  const [predictData, setPredictData] = useState<PredictDataState>({
+    mean_area: '',
+    mean_radius: '',
+    mean_texture: '',
+    mean_perimeter: '',
+    mean_smoothness: '',
+  })
+
+  const dispatch = useDispatch<AppDispatch>()
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setPredictData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }))
+  }
+
+  const handleSubmit = useCallback(
+    async (e: React.FormEvent) => {
+      e.preventDefault()
+
+      const numericData = {
+        mean_area: parseFloat(predictData.mean_area) || 0,
+        mean_radius: parseFloat(predictData.mean_radius) || 0,
+        mean_texture: parseFloat(predictData.mean_texture) || 0,
+        mean_perimeter: parseFloat(predictData.mean_perimeter) || 0,
+        mean_smoothness: parseFloat(predictData.mean_smoothness) || 0,
+      }
+      dispatch(predict(numericData))
+
+      setPredictData({
+        mean_area: '',
+        mean_radius: '',
+        mean_texture: '',
+        mean_perimeter: '',
+        mean_smoothness: '',
+      })
+    },
+    [dispatch, predictData]
+  )
+
   return (
     <Box
       mt="2rem"
       boxShadow="0 4px 6px rgba(0, 0, 0, 0.1)"
       borderBottomLeftRadius="0.5rem"
       borderBottomRightRadius="0.5rem"
+      onSubmit={handleSubmit}
     >
       <Box
         w="full"
-        bgColor="#eee"
+        bgColor="greyLight"
         p="1rem"
         borderTopLeftRadius="0.5rem"
         borderTopRightRadius="0.5rem"
@@ -51,10 +99,13 @@ export const PredictionForm = () => {
               </Field.Label>
               <Input
                 placeholder="Área Média"
-                type="text"
+                type="number"
                 borderColor="input"
                 color="secondary"
                 pl="0.5rem"
+                name="mean_area"
+                value={predictData.mean_area}
+                onChange={(e) => handleInputChange(e)}
               />
               <Field.HelperText></Field.HelperText>
             </Field.Root>
@@ -67,10 +118,13 @@ export const PredictionForm = () => {
               </Field.Label>
               <Input
                 placeholder="Raio Médio"
-                type="text"
+                type="number"
                 borderColor="input"
                 color="secondary"
                 pl="0.5rem"
+                name="mean_radius"
+                value={predictData.mean_radius}
+                onChange={(e) => handleInputChange(e)}
               />
               <Field.HelperText></Field.HelperText>
             </Field.Root>
@@ -83,10 +137,13 @@ export const PredictionForm = () => {
               </Field.Label>
               <Input
                 placeholder="Textura Média"
-                type="text"
+                type="number"
                 borderColor="input"
                 color="secondary"
                 pl="0.5rem"
+                name="mean_texture"
+                value={predictData.mean_texture}
+                onChange={(e) => handleInputChange(e)}
               />
               <Field.HelperText></Field.HelperText>
             </Field.Root>
@@ -99,10 +156,13 @@ export const PredictionForm = () => {
               </Field.Label>
               <Input
                 placeholder="Perímetro Médio"
-                type="text"
+                type="number"
                 borderColor="input"
                 color="secondary"
                 pl="0.5rem"
+                name="mean_perimeter"
+                value={predictData.mean_perimeter}
+                onChange={(e) => handleInputChange(e)}
               />
               <Field.HelperText></Field.HelperText>
             </Field.Root>
@@ -115,10 +175,13 @@ export const PredictionForm = () => {
               </Field.Label>
               <Input
                 placeholder="Suavidade Média"
-                type="text"
+                type="number"
                 borderColor="input"
                 color="secondary"
                 pl="0.5rem"
+                name="mean_smoothness"
+                value={predictData.mean_smoothness}
+                onChange={(e) => handleInputChange(e)}
               />
               <Field.HelperText></Field.HelperText>
             </Field.Root>
