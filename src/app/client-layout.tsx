d035acaf4f1e1925d3475dc97ba4a71013d221a7
Achette <i.achette@live.com>
2025-05-20 10:16:10 -0300

    feat: configura redux store e provider

--- src/app/client-layout.tsx
+++ src/app/client-layout.tsx
@@ -4,6 +4,8 @@ import { usePathname } from 'next/navigation'
 import { Flex } from '@chakra-ui/react'
 import { Header } from '@/components'
 import { Provider } from '@/components/ui/provider'
+import { Provider as ReduxProvider } from 'react-redux'
+import { store } from '@/redux/store'
 
 export default function ClientLayout({
   children,
@@ -15,22 +17,24 @@ export default function ClientLayout({
   const hideStyles = ['/dash', '/new-prediction'].includes(pathname)
 
   return (
-    <Provider>
-      {!hideHeader && <Header />}
-      <Flex
-        bg={
-          !hideStyles
-            ? 'linear-gradient(#e84c8835,rgba(194, 58, 108, 0.09))'
-            : undefined
-        }
-        h={hideStyles ? '' : '100vh'}
-        w="100%"
-        overflowX="hidden"
-        justifyContent="center"
-        alignItems="center"
-      >
-        {children}
-      </Flex>
-    </Provider>
+    <ReduxProvider store={store}>
+      <Provider>
+        {!hideHeader && <Header />}
+        <Flex
+          bg={
+            !hideStyles
+              ? 'linear-gradient(#e84c8835,rgba(194, 58, 108, 0.09))'
+              : undefined
+          }
+          h={hideStyles ? '' : '100vh'}
+          w="100%"
+          overflowX="hidden"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {children}
+        </Flex>
+      </Provider>
+    </ReduxProvider>
   )
 }
