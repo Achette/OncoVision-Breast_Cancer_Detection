805678e67c9103b0de9d6ac3f6c308405243d7d5
Achette <i.achette@live.com>
2025-05-20 10:19:19 -0300

    feat: adiciona novas interfaces

--- src/models/index.ts
+++ src/models/index.ts
@@ -19,3 +19,23 @@ export interface HistoryItem {
 export interface HistoryPayload {
   history: HistoryItem[]
 }
+
+export interface PredictDataState {
+  mean_area: string
+  mean_perimeter: string 
+  mean_radius: string 
+  mean_smoothness: string 
+  mean_texture: string 
+}
+
+export interface PredictData {
+  mean_area: number 
+  mean_perimeter: number 
+  mean_radius: number 
+  mean_smoothness: number 
+  mean_texture: number 
+}
+
+export interface PredictResult {
+  prediction: string | null
+}
