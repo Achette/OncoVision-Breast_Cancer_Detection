5da8ddde0ef4ab279c71fd7d83cbe3f8f321309e
Achette <i.achette@live.com>
2025-05-25 13:10:38 -0300

    feat: exporta componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export * from "./header/index"
 export * from "./table/index"
 export * from "./badge/index"
-export * from './predctionForm/index'
+export * from './predction-form/index'
 export * from './prediction-result/index'
+export * from './loading-spinner/index'
